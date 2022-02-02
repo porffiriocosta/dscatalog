d406206d0ec79b99a1ca3e7ad4bf82d5e16e260e
Reginaldo <porffiriocosta@gmail.com>
2022-02-02 15:42:37 -0300

    Criação dos componentes button, detalhes e rotas

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,12 +1,11 @@
-import './assets/styles/custom.scss';
+import 'assets/styles/custom.scss';
 import './App.css';
-import Home from 'pages/Home';
+
+import Routes from 'Routes';
 
 function App() {
   return (
-    <>
-      <Home />
-    </>
+    <Routes />
   );
 }
 
